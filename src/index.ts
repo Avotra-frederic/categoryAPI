44687704173c435bdc1b00e24a0a9c144ae5b9e7
Avotra-frederic <fred.avotra@gmail.com>
2025-02-08 14:16:07 +0300

    last commit

--- src/index.ts
+++ src/index.ts
@@ -6,41 +6,44 @@ import Category from "./model/category.model";
 const PORT = process.env.PORT || 0;
 
 mongoDBConnection().then(async()=>{
-    // try {
-    //     for (const element of data) {
-    //       if (element.Parent != null) {
-    //         const parent = await Category.findOne({ slug: element.Parent });
-    //         if (parent) {
-    //           let childCategory = await Category.findOne({ slug: element.Category });
-    //           if (!childCategory) {
-    //             childCategory = new Category({
-    //               name: element.Category,
-    //               slug: element.Category,
-    //               parent: parent._id,
-    //             });
-    //             await childCategory.save();
-    //             await Category.findByIdAndUpdate(
-    //               parent._id,
-    //               { $push: { children: childCategory._id } },
-    //               { new: true }
-    //             );
-    //           }
-    //         }
-    //       } else {
-    //         let category = await Category.findOne({ slug: element.Category });
-    //         if (!category) {
-    //           category = new Category({
-    //             name: element.Category,
-    //             slug: element.Category,
-    //             parent: null,
-    //           });
-    //           await category.save();
-    //         }
-    //       }
-    //     }
-    //   } catch (error) {
-    //     console.error("Erreur lors de l'importation des catégories :", error);
-    //   }
+    const count = await Category.countDocuments();
+    if(count === 0){
+        try {
+            for (const element of data) {
+              if (element.Parent != null) {
+                const parent = await Category.findOne({ slug: element.Parent });
+                if (parent) {
+                  let childCategory = await Category.findOne({ slug: element.Category });
+                  if (!childCategory) {
+                    childCategory = new Category({
+                      name: element.Category,
+                      slug: element.Category,
+                      parent: parent._id,
+                    });
+                    await childCategory.save();
+                    await Category.findByIdAndUpdate(
+                      parent._id,
+                      { $push: { children: childCategory._id } },
+                      { new: true }
+                    );
+                  }
+                }
+              } else {
+                let category = await Category.findOne({ slug: element.Category });
+                if (!category) {
+                  category = new Category({
+                    name: element.Category,
+                    slug: element.Category,
+                    parent: null,
+                  });
+                  await category.save();
+                }
+              }
+            }
+          } catch (error) {
+            console.error("Erreur lors de l'importation des catégories :", error);
+          }
+    }
     app.listen(PORT, () => {
         console.log(`Server running on http://localhost:${PORT}`);
     });
